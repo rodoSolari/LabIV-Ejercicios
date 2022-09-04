b4762fc40464e9498e101c81f7c034a2da1f179d
rodoSolari <63050260+rodoSolari@users.noreply.github.com>
2022-09-04 20:03:12 -0300

    Add files via upload

---
+++ ejercicioClase01/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './Vistas/bienvenido/bienvenido.component';
+import { LoginComponent } from './Vistas/login/login.component';
+import { ErrorComponent } from './Vistas/error/error.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
